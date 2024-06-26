a38eeccd79891ed70ef9d543551f4e26281dc9ef
ebukaharry <54277850+ebukaharry@users.noreply.github.com>
2024-06-26 13:04:59 +0100

    Fixed video autoplay issue

--- src/components/Spotlight.vue
+++ src/components/Spotlight.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="md:flex md:flex-row-reverse justify-between xl:gap-28">
         <div class="spotlight-video w-[100%] md:w-[50%] max-h-[100%] mb-10 ">
-            <video src="../assets/media/spotlight.mp4" autoplay muted loop></video>
+            <video src="../assets/media/spotlight.mp4" autoplay muted playsinline loop></video>
         </div>
         <div class="spotlight-text md:w-[45%] lg:w-[45%] xl:w-[50%]">
             <h1 class="text-[40px] md:text-[46px] lg:text-[64px] xl:text-[6.55rem] tracking-tighter lg:tracking-[-0.08em] leading-[2.2rem] md:leading-[.9] lg:leading-[3.4rem] xl:leading-[5.8rem] font-[SctoGroteskA] font-semibold text-[#252422] uppercase">BASIC/DEPT® <br> helps brands ● connect w/ culture</h1>
