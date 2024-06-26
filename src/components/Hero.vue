94811ed1e5f5c342521793a9780df8d68da2c2fa
Ebuka Harry <ebukaharry6@gmail.com>
2024-06-26 12:12:53 +0100

    Fixed cursor issue for mobile devices

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,14 @@
 <template id="hero-body">
     <!-- <div  class="cursor bg-black w-3 h-3 rounded-full select-none pointer-events-none" data-cursor></div> -->
-    <div class="cursor-outline bg-white w-[8rem] h-[8rem] rounded-full select-none pointer-events-none text-center text-[13px] font-[SctoGroteskA] font-bold flex justify-center items-center" data-cursor-outline>WATCH<br>REEL</div>
+    <div class="wrapper">
+       <div class="cursor bg-white w-[5rem] h-[5rem] rounded-full select-none pointer-events-none text-center text-[10px] font-[SctoGroteskA] font-bold flex xl:hidden justify-center items-center">WATCH<br>REEL</div>
+
+       <div class="cursor-outline bg-white w-[8rem] h-[8rem] rounded-full select-none pointer-events-none text-center text-[13px] font-[SctoGroteskA] font-bold flex justify-center items-center" data-cursor-outline>WATCH<br>REEL</div>
     <video autoplay muted loop class="hero-vid" >
-        <source src="../assets/media/hero-vid.mp4" type="video/mp4" @mouseenter="handleMouseEnter">
-    </video>
+        <source src="../assets/media/hero-vid.mp4" type="video/mp4">
+    </video> 
+    </div>
+    
 
 </template>
 
@@ -16,7 +21,6 @@ export default {
    },
 
    mounted() {
-    this.cursor = document.querySelector('[data-cursor]');
     this.cursorOutline = document.querySelector('[data-cursor-outline]');
 
     window.addEventListener('mousemove', this.handleMouseMove);
@@ -45,8 +49,11 @@ export default {
 </script>
 
 <style scoped>
+    .wrapper {
+        transition: all 0.5s cubic-bezier(0.25, 1, 0.3, 1);
+    transform-origin: 90% center;
+    }
    .hero-vid {
-    /* position: fixed; */
     cursor: none;
     overflow: hidden;
     right: 0;
@@ -54,28 +61,49 @@ export default {
     min-width: 100vw;
     min-height: 100vh;
     object-fit: cover;
-    /* opacity: 0.4; */
-    transition: all 0.5s cubic-bezier(0.25, 1, 0.3, 1);
-    transform-origin: 90% center;
    }
 
-  .cursor-outline {
+  .cursor-outline, .cursor {
+    display: none;
     transition: 0.5s cubic-bezier(0.75, -1.27, 0.3, 2.33) transform, 0.2s cubic-bezier(0.75, -1.27, 0.3, 2.33) opacity;
     transform: translate(-50%, -50%);
     position: fixed;
-    top: 0;
-    left: 0;
+    top: 50%;
+    left: 50%;
    }
 
-   .cursor-outline::before, .cursor-outline::after {
+   .cursor {
+    position: absolute;
+    display: flex;
+   }
+
+   .cursor-outline::before, .cursor-outline::after, .cursor::before, .cursor::after {
     content: "BASIC/DEPT®";
     position: absolute;
     bottom: -30px;
     color: #fff;
    }
 
-   .cursor-outline::after {
+   .cursor::before {
+    bottom: -20px;
+   }
+
+   .cursor-outline::after, .cursor::after {
     content: "2010-∞";
     bottom: -50px;
+   }
+
+   .cursor::after {
+    bottom: -30px;
+   }
+
+   @media screen and (min-width: 600px) {
+    .cursor-outline {
+        display: flex;
+    }
+
+    .cursor {
+        display: none;
+    }
    }
 </style>
